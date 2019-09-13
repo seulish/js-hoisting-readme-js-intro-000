82c7413620e12276f30bbe142fb9d3e8a699765e
Leah Lee <leah@iterable.com>
2019-09-13 07:26:45 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -21,9 +21,9 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
+  sayMy();
   function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
-  return sayMy();
 }
